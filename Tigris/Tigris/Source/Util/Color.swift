08aa5c0126fc74b36fb62dc493bf619246521fea
KimKiYoung <rlarldud6482@naver.com>
2021-12-17 21:06:35 +0900

    Util 세팅

---
+++ Tigris/Tigris/Source/Util/Color.swift
@@ -0,0 +1,24 @@
+//
+//  Color.swift
+//  Tigris
+//
+//  Created by 김기영 on 2021/12/17.
+//
+
+import UIKit
+
+protocol Point {
+    static func pointHex(hex: String) -> UIColor
+}
+
+extension Point {
+    static func pointHex(hex: String) -> UIColor {
+        var rgbValue: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&rgbValue)
+        return UIColor(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                       green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                       blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                       alpha: CGFloat(1.0)
+        )
+    }
+}
---
+++ Tigris/Tigris/Source/Util/Font.swift
@@ -0,0 +1,8 @@
+//
+//  Font.swift
+//  Tigris
+//
+//  Created by 김기영 on 2021/12/17.
+//
+
+import Foundation
