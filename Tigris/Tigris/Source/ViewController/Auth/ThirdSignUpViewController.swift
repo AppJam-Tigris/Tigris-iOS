a240ab3707e3923089e12944912149b4afa6d715
KimKiYoung <rlarldud6482@naver.com>
2021-12-19 07:36:27 +0900

    세번 회원가입 뷰컨트롤러

--- Tigris/Tigris/Source/ViewController/Auth/ThirdSignUpViewController.swift
+++ Tigris/Tigris/Source/ViewController/Auth/ThirdSignUpViewController.swift
@@ -1,99 +1,73 @@
 //
-//  SignUpViewController.swift
+//  FirstSignUpViewController.swift
 //  Tigris
 //
 //  Created by 김기영 on 2021/12/18.
 //
 
+import Foundation
+
 import UIKit
-import DropDown
 
 class ThirdSignUpViewController: BaseViewController {
+    
+    private let viewModel = ThirdSignUpViewModel()
+    var birth = String()
+    var gender = String()
+    var nationality = String()
+    var location = Location(address: nil, road_name: nil, details_address: nil)
     
     private let logo = UIImageView().then {
         $0.image = .init(named: "logoImg")
         $0.contentMode = .scaleAspectFit
     }
     
-    private let label = UILabel().then {
-        $0.text = "편한 코로나 선별진료소"
-        $0.font = .init(name: Font.medium.rawValue, size: 14)
-    }
-    
-    private let birth = AuthView().then {
-        $0.txt.placeholder = "Birthday(생년월일)"
+    private let name = AuthView().then {
+        $0.txt.placeholder = "Name(이름)"
         $0.txt.font = .init(name: Font.regular.rawValue, size: 12)
         $0.eyesBtn.isHidden = true
     }
     
-    private let gender = AuthView().then {
-        $0.txt.placeholder = "Gender(성별)"
+    private let id = AuthView().then {
+        $0.txt.placeholder = "ID(아이디)"
         $0.txt.font = .init(name: Font.regular.rawValue, size: 12)
         $0.eyesBtn.isHidden = true
     }
     
-    private let genderBtn = UIButton(type: .system).then {
-        $0.setTitle("성별", for: .normal)
-        $0.setImage(.init(systemName: "chevron.down", withConfiguration: UIImage.SymbolConfiguration(pointSize: 10, weight: .regular, scale: .small)), for: .normal)
-        $0.tintColor = .gray
-        $0.semanticContentAttribute = .forceRightToLeft
+    private let idCheckBtn = UIButton(type: .system).then {
+        $0.setTitle("중복 확인", for: .normal)
         $0.setTitleColor(.black, for: .normal)
-        $0.titleLabel?.font = .init(name: Font.regular.rawValue, size: 8)
+        $0.titleLabel?.font = .systemFont(ofSize: 8)
         $0.layer.borderWidth = 0.5
         $0.backgroundColor = .white
     }
     
-    private let nationality = AuthView().then {
-        $0.txt.placeholder = "Nationality(국적)"
+    private let idError = UILabel().then {
+        $0.font = .init(name: Font.regular.rawValue, size: 8)
+        $0.textColor = .red
+    }
+    
+    private let pw = AuthView().then {
+        $0.txt.placeholder = "PASSWORD(비밀번호)"
         $0.txt.font = .init(name: Font.regular.rawValue, size: 12)
-        $0.eyesBtn.isHidden = true
     }
     
-    private let nationalityBtn = UIButton(type: .system).then {
-        $0.setTitle("국적", for: .normal)
-        $0.setTitleColor(.black, for: .normal)
-        $0.setImage(.init(systemName: "chevron.down", withConfiguration: UIImage.SymbolConfiguration(pointSize: 10, weight: .regular, scale: .small)), for: .normal)
-        $0.tintColor = .gray
-        $0.semanticContentAttribute = .forceRightToLeft
-        $0.titleLabel?.font = .init(name: Font.regular.rawValue, size: 8)
-        $0.layer.borderWidth = 0.5
-        $0.backgroundColor = .white
-    }
-    
-    private let location = AuthView().then {
-        $0.txt.placeholder = "Location(주소 찾기)"
+    private let checkPw = AuthView().then {
+        $0.txt.placeholder = "PASSWORD CHECK(비밀번호 확인)"
         $0.txt.font = .init(name: Font.regular.rawValue, size: 12)
-        $0.eyesBtn.isHidden = true
     }
     
-    private let locationBtn = UIButton(type: .system).then {
-        $0.setTitle("주소 찾기", for: .normal)
-        $0.setTitleColor(.black, for: .normal)
-        $0.setImage(.init(systemName: "chvron.down"), for: .normal)
-        $0.semanticContentAttribute = .forceRightToLeft
-        $0.titleLabel?.font = .init(name: Font.regular.rawValue, size: 8)
-        $0.layer.borderWidth = 0.5
-        $0.backgroundColor = .white
+    private let pwError = UILabel().then {
+        $0.font = .init(name: Font.regular.rawValue, size: 8)
+        $0.textColor = .red
     }
     
-    private let signupBtn = UIButton(type: .system).then {
-        $0.setTitle("회원가입", for: .normal)
+    private let nextBtn = UIButton(type: .system).then {
+        $0.setTitle("다음", for: .normal)
         $0.setTitleColor(.white, for: .normal)
         $0.backgroundColor = MainColor.darkBlue
         $0.layer.cornerRadius = 15
         $0.titleLabel?.font = .init(name: Font.medium.rawValue, size: 16)
-    }
-    
-    private let genderDrop = DropDown().then {
-        $0.width = 48
-        $0.dataSource = ["남자", "여자"]
-    }
-    
-    private let nationalityDrop = DropDown().then {
-        $0.width = 48
-        $0.dataSource = ["내국인", "외국인"]
-        $0.textFont = .systemFont(ofSize: 12)
-        $0.cornerRadius = 5
     }
     
     override func setNavigation() {
@@ -101,107 +75,111 @@ class ThirdSignUpViewController: BaseViewController {
     }
     
     override func configureUI() {
-        genderDrop.anchorView = genderBtn
-        nationalityDrop.anchorView = nationalityBtn
+        [logo, name, id, idCheckBtn, pw, checkPw, nextBtn].forEach{view.addSubview($0)}
+    }
+    
+    override func bindViewModel() {
+        let input = ThirdSignUpViewModel.Input(name: name.txt.rx.text.orEmpty.asDriver(onErrorJustReturn: ""),
+                                               id: id.txt.rx.text.orEmpty.asDriver(),
+                                               pw: pw.txt.rx.text.orEmpty.asDriver(),
+                                               checkPw: checkPw.txt.rx.text.orEmpty.asDriver(),
+                                               doneTap: idCheckBtn.rx.tap.asDriver())
+        let output = viewModel.transform(input)
         
-        [logo, label, birth, gender, genderBtn,
-        nationality, nationalityBtn, location, locationBtn, signupBtn].forEach{view.addSubview($0)}
+        output.isEnable.drive(nextBtn.rx.isEnabled).disposed(by: disposeBag)
         
-        genderDrop.selectionAction = { [unowned self] (index: Int, item: String) in
-                genderBtn.setTitle(item, for: .normal)
-                gender.txt.text = item
-        }
+        output.checkIdResult.asDriver().drive(onNext: { enable in
+            if enable {
+                self.idError.text = ""
+                self.nextBtn.isEnabled = enable
+                let alert = UIAlertController.init(title: "사용 가능한 아이디입니다.", message: "사용가능합니다.", preferredStyle: .alert)
+                let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                alert.addAction(ok)
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                self.idError.text = "아이디가 중복됩니다."
+                self.nextBtn.isEnabled = enable
+            }
+        }).disposed(by: disposeBag)
         
-        nationalityDrop.selectionAction = {[unowned self] (index: Int, item: String) in
-            nationality.txt.text = item
-            nationalityBtn.setTitle(item, for: .normal)
-        }
     }
     
     override func setBtn() {
-        
-        genderBtn.rx.tap.subscribe(onNext: {
-            self.genderDrop.show()
+        nextBtn.rx.tap.subscribe(onNext: { [unowned self] _ in
+            let vc = CheckPhoneViewController()
+            vc.name = name.txt.text!
+            vc.id = id.txt.text!
+            vc.pw = pw.txt.text!
+            vc.location.address = location.address
+            vc.location.road_name = location.road_name
+            vc.location.details_address = location.details_address
+            vc.birth = birth
+            vc.gender  = gender
+            vc.nationality = nationality
+            
+            self.navigationController?.pushViewController(vc, animated: true)
         }).disposed(by: disposeBag)
-        
-        nationalityBtn.rx.tap.subscribe(onNext: {
-            self.nationalityDrop.show()
-        }).disposed(by: disposeBag)
-        
     }
     
     override func setUpConstraints() {
+        idCheckBtn.layer.cornerRadius = 5
         
-        [genderDrop, nationalityDrop].forEach{
-            $0.bottomOffset = CGPoint(x: 0, y:($0.anchorView?.plainView.bounds.height)!)}
-        
-        [genderBtn, nationalityBtn, locationBtn].forEach{$0.layer.cornerRadius = 5}
-        
-        [birth, gender, nationality, location].forEach{
+        [name, id, pw, checkPw].forEach{
             $0.layer.addBorder([.bottom], color: .gray, width: 1)
         }
         
         logo.snp.makeConstraints {
             $0.top.equalToSuperview().inset(100)
             $0.centerX.equalToSuperview()
-            $0.width.equalTo(116)
-            $0.height.equalTo(38)
+            $0.width.equalTo(123)
+            $0.height.equalTo(74)
         }
         
-        label.snp.makeConstraints {
-            $0.top.equalTo(logo.snp.bottom).offset(18)
-            $0.centerX.equalToSuperview()
-        }
-        
-        birth.snp.makeConstraints {
-            $0.top.equalTo(label.snp.bottom).offset(61)
+        name.snp.makeConstraints {
+            $0.top.equalTo(logo.snp.bottom).offset(61)
             $0.leading.trailing.equalToSuperview().inset(44)
             $0.height.equalTo(30)
         }
         
-        gender.snp.makeConstraints {
-            $0.top.equalTo(birth.snp.bottom).offset(38)
+        id.snp.makeConstraints {
+            $0.top.equalTo(name.snp.bottom).offset(38)
             $0.leading.trailing.equalToSuperview().inset(44)
             $0.height.equalTo(30)
         }
         
-        genderBtn.snp.makeConstraints {
-            $0.centerY.equalTo(gender)
-            $0.trailing.equalTo(gender.snp.trailing).inset(5)
+        idCheckBtn.snp.makeConstraints {
+            $0.centerY.equalTo(id)
+            $0.trailing.equalTo(id.snp.trailing).inset(5)
             $0.width.equalTo(48)
             $0.height.equalTo(12)
         }
         
-        nationality.snp.makeConstraints {
-            $0.top.equalTo(gender.snp.bottom).offset(38)
+        pw.snp.makeConstraints {
+            $0.top.equalTo(id.snp.bottom).offset(38)
             $0.leading.trailing.equalToSuperview().inset(44)
             $0.height.equalTo(30)
         }
         
-        nationalityBtn.snp.makeConstraints {
-            $0.centerY.equalTo(nationality)
-            $0.trailing.equalTo(gender.snp.trailing).inset(5)
-            $0.width.equalTo(48)
-            $0.height.equalTo(12)
-        }
-        
-        location.snp.makeConstraints {
-            $0.top.equalTo(nationality.snp.bottom).offset(38)
+        checkPw.snp.makeConstraints {
+            $0.top.equalTo(pw.snp.bottom).offset(38)
             $0.leading.trailing.equalToSuperview().inset(44)
             $0.height.equalTo(30)
         }
         
-        locationBtn.snp.makeConstraints {
-            $0.centerY.equalTo(location)
-            $0.trailing.equalTo(location.snp.trailing).inset(5)
-            $0.width.equalTo(48)
-            $0.height.equalTo(12)
-        }
-        
-        signupBtn.snp.makeConstraints {
-            $0.top.equalTo(location.snp.bottom).offset(78)
+        nextBtn.snp.makeConstraints {
+            $0.top.equalTo(checkPw.snp.bottom).offset(78)
             $0.leading.trailing.equalToSuperview().inset(44)
             $0.height.equalTo(40)
+        }
+    }
+}
+
+extension ThirdSignUpViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if pw.txt.text != checkPw.txt.text {
+            pwError.text = "비밀번호가 일치하지 않습니다."
+        } else {
+            pwError.text = ""
         }
     }
 }
