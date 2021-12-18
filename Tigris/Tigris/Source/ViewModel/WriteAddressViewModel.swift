eeebf1e5bd2e79925266db400059bdbb81662234
KimKiYoung <rlarldud6482@naver.com>
2021-12-19 07:23:45 +0900

    주소 입력 뷰컨트롤러 유아이 짜기

--- Tigris/Tigris/Source/ViewModel/WriteAddressViewModel.swift
+++ Tigris/Tigris/Source/ViewModel/WriteAddressViewModel.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+class WriteAddressViewModel: ViewModelType {
+    private let disposeBag = DisposeBag()
+    struct Input {
+        let keyWord: Driver<String>
+        let tap: Driver<Void>
+    }
+    
+    struct Output {
+        let posts: BehaviorRelay<[postModel]>
+        let result: PublishRelay<Bool>
+        
+    }
+    
+    func transform(_ input: Input) -> Output {
+        let api = Service()
+        let posts = BehaviorRelay<[postModel]>(value: [])
+        let result = PublishRelay<Bool>()
+        
+        
+        input.tap.asObservable().withLatestFrom(input.keyWord).flatMap{ keyword in
+            api.search(keyword)
+        }.subscribe(onNext: { data, res in
+            switch res {
+            case .ok:
+                posts.accept(data!.posts)
+                result.accept(true)
+            default:
+                result.accept(false)
+            }
+        }).disposed(by: disposeBag)
+        
+        return Output(posts: posts, result: result)
+    }
+}
