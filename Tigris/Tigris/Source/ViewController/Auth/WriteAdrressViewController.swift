78203f50c17ada29fce234c5b1e77a0e39ef72e4
KimKiYoung <rlarldud6482@naver.com>
2021-12-19 07:24:57 +0900

    주소 작성 뷰컨트롤러 유아이 수정

--- Tigris/Tigris/Source/ViewController/Auth/WriteAdrressViewController.swift
+++ Tigris/Tigris/Source/ViewController/Auth/WriteAdrressViewController.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+    let webView = WKWebView()
+    
+    override func viewDidLoad() {
+        view.addSubview(webView)
+        webView.navigationDelegate = self
+
+        let url = URL(string: "https://postcode.pocketlesson.io")!
+        webView.load(URLRequest(url: url))
+    }
+    
+    override func viewDidLayoutSubviews() {
+        webView.snp.makeConstraints {
+            $0.top.equalTo(view.snp.top)
+            $0.leading.trailing.equalTo(view)
+            $0.bottom.equalTo(view.snp.bottom)
+        }
+    }
+
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+      switch navigationAction.request.url?.scheme {
+      case "postcode":
+        if let urlString = navigationAction.request.url?.absoluteString {
+          let components = URLComponents(string: urlString)
+          let address = components?.queryItems?.first { $0.name == "address" }?.value
+          let zonecode = components?.queryItems?.first { $0.name == "zonecode" }?.value
+        let vc = SecondSignUpViewController()
+            vc.adress = address!
+            vc.code = zonecode!
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+        decisionHandler(.cancel)
+
+      default:
+        decisionHandler(.allow)
+      }
+    }
+}
