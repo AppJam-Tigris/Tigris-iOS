a00acae2c392feab59aff552b9d199c73b35ba59
KimKiYoung <rlarldud6482@naver.com>
2021-12-18 22:42:05 +0900

    첫번쨰 회원가입 뷰컨트롤러 유아이

--- Tigris/Tigris/Source/ViewModel/Auth/FirstSignUpViewController.swift
+++ Tigris/Tigris/Source/ViewModel/Auth/FirstSignUpViewController.swift
@@ -5,4 +5,26 @@
 //  Created by 김기영 on 2021/12/18.
 //
 
-import Foundation
+import UIKit
+import RxCocoa
+
+class FirstSignUpViewModel: ViewModelType {
+    
+    struct Input {
+        let phoneNum: Driver<String>
+        let numBtnTap: Driver<Void>
+        let checkNum: Driver<Int>
+        let checkBtnTap: Driver<Void>
+    }
+    
+    struct Output {
+        let result: Driver<Bool>
+    }
+    
+    func transform(_ input: Input) -> Output {
+        let api = Service()
+        let info = Driver.combineLatest(input.phoneNum, input.checkNum)
+        let result = PublishRelay<Bool>()
+        
+    }
+}
