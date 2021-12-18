b3b453e9eb186cdaaf275ee07b30f7504aed1193
KimKiYoung <rlarldud6482@naver.com>
2021-12-18 22:45:03 +0900

    API 추가

--- Tigris/Tigris/Source/Networking/API.swift
+++ Tigris/Tigris/Source/Networking/API.swift
@@ -13,12 +13,14 @@ import Moya
 enum API {
     case signIn(_ id: String, _ password: String)
     case signUp(_ id: String, _ password: String)
+    case postPhoneNum(_ num: String)
+    case checkPhoneNum(_ num: String, _ code: Int)
 }
 
 extension API: TargetType {
     
     var baseURL: URL {
-        URL(string: "")!
+        URL(string: "http://49.247.195.161:8080")!
     }
     
     var path: String {
@@ -27,13 +29,17 @@ extension API: TargetType {
             return "/user/auth"
         case .signUp:
             return "/user"
+        case .postPhoneNum, .checkPhoneNum:
+            return "/user/phone"
         }
     }
     
     var method: Moya.Method {
         switch self {
-        case .signIn, .signUp:
+        case .signIn, .signUp, .postPhoneNum:
             return .post
+        case .checkPhoneNum:
+            return .put
         }
     }
     
@@ -45,12 +51,16 @@ extension API: TargetType {
         case .signUp(let id, let password):
             return .requestParameters(parameters: ["uid": id, "password": password],
                                       encoding: JSONEncoding.prettyPrinted)
+        case .postPhoneNum(let num):
+            return .requestParameters(parameters: ["phone_number": num], encoding: JSONEncoding.prettyPrinted)
+        case .checkPhoneNum(let num, let code):
+            return .requestParameters(parameters: ["phone_number": num, "code": code], encoding: JSONEncoding.prettyPrinted)
         }
     }
     
     var headers: [String : String]? {
         switch self {
-        case .signUp, .signIn:
+        case .signUp, .signIn, .postPhoneNum, .checkPhoneNum:
             return ["Content-Type" : "application/json"]
         }
     }
