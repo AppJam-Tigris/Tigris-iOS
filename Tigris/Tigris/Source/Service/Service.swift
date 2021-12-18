e1d47d413d601e32f59d2991a1c29c71b77081b1
KimKiYoung <rlarldud6482@naver.com>
2021-12-19 08:22:43 +0900

    Service setting

--- Tigris/Tigris/Source/Service/Service.swift
+++ Tigris/Tigris/Source/Service/Service.swift
@@ -26,8 +26,8 @@ final class Service {
             .catch { [unowned self] in return .just(setNetworkError($0))}
     }
     
-    func signUp(_ id: String, _ password: String) -> Single<StatusCode> {
-        return provider.rx.request(.signUp(id, password))
+    func signUp(_ name: String, _ phone_num: String, _ code: String, _ birthday: String, _ gender: String, _ nationlity: String, _ location: Location, _ uid: String, _ password: String) -> Single<StatusCode> {
+        return provider.rx.request(.signUp(name, phone_num, code, birthday, gender, nationlity, location, uid, password))
             .filterSuccessfulStatusCodes()
             .map{ _ -> StatusCode in return .createOk }
             .catch{ [unowned self] in return .just(setNetworkError($0))}
@@ -40,11 +40,29 @@ final class Service {
             .catch{ [unowned self] in return .just(setNetworkError($0))}
     }
     
-    func checkPhoneNum(_ num: String, _ code: Int) -> Single<StatusCode> {
+    func checkPhoneNum(_ num: String, _ code: String) -> Single<StatusCode> {
         return provider.rx.request(.checkPhoneNum(num, code))
             .filterSuccessfulStatusCodes()
             .map{_ -> StatusCode in return .ok}
             .catch{ [unowned self] in return .just(setNetworkError($0))}
+    }
+    
+    func checkId(_ id: String) -> Single<StatusCode> {
+        return provider.rx.request(.checkId(id))
+            .filterSuccessfulStatusCodes()
+            .map{_ -> StatusCode in return .ok}
+            .catch{ [unowned self] in return .just(setNetworkError($0))}
+    }
+    
+    func search(_ keyword: String) -> Single<(posts?, StatusCode)> {
+        return provider.rx.request(.search(keyword))
+            .filterSuccessfulStatusCodes()
+            .map(posts.self)
+            .map{return ($0, .ok)}
+            .catch { error in
+                print(error)
+                return .just((nil, .fail))
+            }
     }
     
     func setNetworkError(_ error: Error) -> StatusCode {
